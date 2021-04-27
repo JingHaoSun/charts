456adfaa6058588c05bbdeb6c4149a0f5d445514
sunjinghao <2806809869@qq.com>
2021-04-27 21:24:08 +0800

    转置、筛选

--- charts.py
+++ charts.py
@@ -15,71 +15,130 @@ db.init_app(app)
 app.config['JSON_AS_ASCII'] = False
 
 
-@app.route('/data', methods=['POST'])
-def data():
-    #sql
-    tablename = request.form['tablename']
+def datasql(tablename, hlzh,type):
     sql = 'select * from ' + tablename
     result = db.session.execute(sql)
     result = result.cursor.fetchall()
     sql2 = 'select * from ' + tablename + '_contrast'
     result2 = db.session.execute(sql2)
     result2 = result2.cursor.fetchall()
-
     #data
-    data = {}
     xLabelList = []
     dataTable = []
-    for re2 in result2:
-        a = {}
-        a['cname'] = re2[1]
-        a['ename'] = re2[2]
-        a['pointer'] = 'xLabel' +str(re2[0])
-        xLabelList.append(a)
-    for re in result:
-        b = {}
-        b['c_yAxis'] = re[0]
-        b['e_yAxis'] = re[1]
-        for i in range(len(result2)):
-            b['xLabel' + str(i)] = re[i + 2]
-        dataTable.append(b)
+    #hlzh = '0'转换
+    if hlzh == '0':
+        index = 0
+        for re1 in result2:
+            b = {}
+            b['c_yAxis'] = re1[1]
+            b['e_yAxis'] = re1[2]
+            for i in range(len(result)):
+                b['xLabel' + str(i)] = result[i][index + 2]
+            index += 1
+            dataTable.append(b)
+        for re2 in result:
+            a = {}
+            a['cname'] = re2[1]
+            a['ename'] = re2[1]
+            a['pointer'] = 'xLabel' + str(re2[0])
+            xLabelList.append(a)
+    #hlzh = '1'不转换
+    elif hlzh == '1':
+        for re2 in result2:
+            a = {}
+            a['cname'] = re2[1]
+            a['ename'] = re2[2]
+            a['pointer'] = 'xLabel' + str(re2[0])
+            xLabelList.append(a)
+        for re in result:
+            b = {}
+            b['c_yAxis'] = re[1]
+            b['e_yAxis'] = re[1]
+            for i in range(len(result2)):
+                b['xLabel' + str(i)] = re[i + 2]
+            dataTable.append(b)
+    return xLabelList,dataTable
 
 
-    #count
-    count = request.form['count']
-    if count == None:
-        count = 5
-    dataTable = dataTable[:int(count)]
+@app.route('/data', methods=['POST'])
+def datamethod():
+    #data
+    datarequest = request.form.to_dict()
+    tablename = ''
+    count = 5
+    search_list = ''
+    hlzh = 1
+    type = ''
+    for key in datarequest:
+        if key == 'tablename':
+            tablename = datarequest[key]
+        if key == 'count':
+            count = datarequest[key]
+        if key == 'search_list':
+            search_list = datarequest[key]
+        if key == 'hlzh':
+            hlzh = datarequest[key]
+        if key == 'type':
+            type = datarequest[key]
+    #sql
+    data = {}
+    tablename = request.form['tablename']
+    if tablename is None:
+        return {"code": 404, "data": "error"}
+    #hlzh
+    hlzh = request.form['hlzh']
+    xLabelList,dataTable = datasql(tablename, hlzh,type)
 
-    #Data filtering
-    type = request.form['type']
+    # Data filtering
     search_list = request.form['search_list']
-
-    items = json.loads(search_list).items()
     dtpd = pd.DataFrame(dataTable)
-    if type == 'column_a':
-        for key, value in items:
-            for elist in xLabelList:
-                if elist.get('ename') == key:
-                    pointer = elist.get('pointer')
-                    break
+    dtpdtemp = dtpd.loc[:,['c_yAxis','e_yAxis']]
+    dtpd = dtpd.set_index('e_yAxis')
+    dtpdtemp.index = dtpd.index
+    dtpd = dtpd.drop(labels='c_yAxis',axis=1, index=None, columns=None, inplace=False)
 
-            print(dtpd)
-            for v in value:
-                if 'gt' in v:
-                    dtpd = dtpd.loc[(dtpd[pointer] > int(v['gt'])),:]
-                elif 'lt' in v:
-                    dtpd = dtpd.loc[(dtpd[pointer] < int(v['lt'])),:]
-                elif 'le' in v:
-                    dtpd = dtpd.loc[(dtpd[pointer] <= int(v['le'])),:]
-                elif 'ge' in v:
-                    dtpd = dtpd.loc[(dtpd[pointer] >= int(v['ge'])),:]
+    #按行筛选
+    if search_list != '':
+        items = json.loads(search_list).items()
+        if type == 'row':
+            for key, value in items:
+                for elist in xLabelList:
+                    if elist.get('ename') == key:
+                        pointer = elist.get('pointer')
+                        break
+                for v in value:
+                    if 'gt' in v:
+                        dtpd = dtpd.loc[(dtpd[pointer] > int(v['gt'])), :]
+                    elif 'lt' in v:
+                        dtpd = dtpd.loc[(dtpd[pointer] < int(v['lt'])), :]
+                    elif 'le' in v:
+                        dtpd = dtpd.loc[(dtpd[pointer] <= int(v['le'])), :]
+                    elif 'ge' in v:
+                        dtpd = dtpd.loc[(dtpd[pointer] >= int(v['ge'])), :]
+        elif type == 'column':
+            for key, value in items:
+                print(key,value)
+                for v in value:
+                    if 'gt' in v:
+                        dtpd = dtpd.loc[ : ,(dtpd.xs(key,axis = 0) > int(v['gt']))]
+                    elif 'lt' in v:
+                        dtpd = dtpd.loc[: ,(dtpd.xs(key,axis = 0)<int(v['lt']))]
+                    elif 'le' in v:
+                        dtpd = dtpd.loc[: ,(dtpd.xs(key,axis = 0)<=int(v['le']))]
+                    elif 'ge' in v:
+                        dtpd = dtpd.loc[: ,(dtpd.xs(key,axis = 0)>=int(v['ge']))]
 
-
+    dtpd = dtpdtemp.join(dtpd)
+    dtpd = dtpd.dropna(axis=0, how='any')
+    #count
+    if type == 'row':
+        dtpd = dtpd.iloc[:, 0:int(count) + 2]
+    if type == 'column':
+        dtpd = dtpd.iloc[:int(count)]
     data['xLabelList'] = xLabelList
-    data['dataTable'] = dtpd.values.tolist()
+    data['dataTable'] = dtpd.to_dict(orient='records')
+    return {"code": 200, "data": data}
 
-    return {"code":200,"data":data}
 
 @app.route('/login',methods=['POST'])
 def login():
